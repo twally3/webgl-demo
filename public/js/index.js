ae40ac252006341fc1867b7182d89b909d32518c
Max Taylor <max@cakerstream.com>
2019-02-12 14:40:03 +0000

    Added colour buffer to shade triangle

--- public/js/index.js
+++ public/js/index.js
@@ -5,19 +5,34 @@ if (!gl) throw new Error('WebGL is not supported!');
 
 const vertexData = [
 	0, 1, 0,
-	1, 0, 0,
-	-1, 0, 0
+	1, -1, 0,
+	-1, -1, 0
 ];
 
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+const colorData = [
+	1, 0, 0,
+	0, 1, 0,
+	0, 0, 1
+];
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
+	precision mediump float;
+
 	attribute vec3 position;
+	attribute vec3 color;
+	varying vec3 vColor;
 
 	void main() {
+		vColor = color;
 		gl_Position = vec4(position, 1);
 	}
 `);
@@ -25,8 +40,12 @@ gl.compileShader(vertexShader);
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, `
+	precision mediump float;
+
+	varying vec3 vColor;
+
 	void main() {
-		gl_FragColor = vec4(1, 0, 0, 1);
+		gl_FragColor = vec4(vColor, 1);
 	}
 `);
 gl.compileShader(fragmentShader);
@@ -38,7 +57,13 @@ gl.linkProgram(program);
 
 const positionLocation = gl.getAttribLocation(program, `position`);
 gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+const colorLocation = gl.getAttribLocation(program, `color`);
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
 gl.useProgram(program);
 gl.drawArrays(gl.TRIANGLES, 0, 3);
