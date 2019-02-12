56b6e1574567a329fac3fd6394bbde4fd0c83d3d
Max Taylor <max@cakerstream.com>
2019-02-12 20:05:05 +0000

    Made a point cloud

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const { mat4 } = window.glMatrix;
+const { mat4, vec3 } = window.glMatrix;
 
 const canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth;
@@ -9,89 +9,31 @@ const gl = canvas.getContext('webgl');
 if (!gl) throw new Error('WebGL is not supported!');
 
 
-const vertexData = [
-	//  Clockwise wind - FLBRTB
-	// Front 
-	0.5, 0.5, 0.5,
-	0.5, -0.5, 0.5,
-	-0.5, 0.5, 0.5,
-	-0.5, 0.5, 0.5,
-	0.5, -0.5, 0.5,
-	-0.5, -0.5, 0.5,
-
-	// Left
-	-0.5, 0.5, 0.5,
-	-0.5, -0.5, 0.5,
-	-0.5, 0.5, -0.5,
-	-0.5, 0.5, -0.5,
-	-0.5, -0.5, 0.5,
-	-0.5, -0.5, -0.5,
-
-	//Back
-	-0.5, 0.5, -0.5,
-	-0.5, -0.5, -0.5,
-	0.5, 0.5, -0.5,
-	0.5, 0.5, -0.5,
-	-0.5, -0.5, -0.5,
-	0.5, -0.5, -0.5,
-
-	// Right
-	0.5, 0.5, -0.5,
-	0.5, -0.5, -0.5,
-	0.5, 0.5, 0.5, 
-	0.5, 0.5, 0.5, 
-	0.5, -0.5, -0.5,
-	0.5, -0.5, 0.5,
-
-	// Top
-	0.5, 0.5, 0.5, 
-	0.5, 0.5, -0.5,
-	-0.5, 0.5, 0.5,
-	-0.5, 0.5, 0.5,
-	0.5, 0.5, -0.5,
-	-0.5, 0.5, -0.5,
-
-	// Bottom
-	0.5, -0.5, 0.5,
-	0.5, -0.5, -0.5,
-	-0.5, -0.5, 0.5,
-	-0.5, -0.5, 0.5,
-	0.5, -0.5, -0.5,
-	-0.5, -0.5, -0.5
-];
-
-function randomColor() {
-	return [Math.random(), Math.random(), Math.random()];
-}
-
-let colorData = [];
-for (let face = 0; face < vertexData.length / 3; face++) {
-	let faceColor = randomColor();
-	for (let vertex = 0; vertex < 6; vertex++) {
-		colorData.push(...faceColor);
+function spherePointCloud(pointCount) {
+	let points = [];
+	for (let i = 0; i < pointCount; i++) {
+		points.push(...vec3.normalize(vec3.create(), [Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5]));
 	}
+	return points;
 }
+
+const vertexData = spherePointCloud(1e6);
 
 const positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
-
-const colorBuffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
 	precision mediump float;
 
 	attribute vec3 position;
-	attribute vec3 color;
 	varying vec3 vColor;
 
 	uniform mat4 matrix;
 
 	void main() {
-		vColor = color;
+		vColor = vec3(position.xy, 1);
 		gl_Position = matrix * vec4(position, 1);
 	}
 `);
@@ -119,11 +61,6 @@ gl.enableVertexAttribArray(positionLocation);
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
 
-const colorLocation = gl.getAttribLocation(program, `color`);
-gl.enableVertexAttribArray(colorLocation);
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
-
 gl.useProgram(program);
 gl.enable(gl.DEPTH_TEST);
 
@@ -131,27 +68,30 @@ const uniformLocations = {
 	matrix: gl.getUniformLocation(program, `matrix`)
 };
 
+const modelMatrix = mat4.create();
+const viewMatrix = mat4.create();
 const projectionMatrix = mat4.create();
 mat4.perspective(projectionMatrix, 75 * Math.PI / 180, canvas.width / canvas.height, 1e-4, 1e4);
 
 const mvMatrix = mat4.create();
 const mvpMatrix = mat4.create();
-const modelMatrix = mat4.create();
-const viewMatrix = mat4.create();
 
-mat4.translate(modelMatrix, modelMatrix, [-1.5, 0, -2]);
-mat4.translate(viewMatrix, viewMatrix, [-3, 0, 1]);
+mat4.translate(modelMatrix, modelMatrix, [0, 0, 0]);
+
+mat4.translate(viewMatrix, viewMatrix, [0, 0.1, 5]);
 mat4.invert(viewMatrix, viewMatrix);
 
 function animate() {
 	requestAnimationFrame(animate);
 
+	mat4.rotateY(modelMatrix, modelMatrix, 0.03);
+
 	mat4.multiply(mvMatrix, viewMatrix, modelMatrix);
 	mat4.multiply(mvpMatrix, projectionMatrix, mvMatrix);
 
 	gl.uniformMatrix4fv(uniformLocations.matrix, false, mvpMatrix);
-	
-	gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 3);
+
+	gl.drawArrays(gl.POINTS, 0, vertexData.length / 3);
 }
 
 animate();
